aa921b9466b72d09978c4c1979cdddd15d679edf
Pathan Ayan khan <ayankhan782204@gmail.com>
2025-06-19 16:42:18 +0530

    Create Hello.php

---
+++ Hello.php
@@ -0,0 +1,19 @@
+ <?php
+echo "Hello, welcome to PHP Basics AAp ka bahot bahot shukriya!<br>";
+print "Let's learn how PHP works step by step Aao seekhe.<br>";
+
+$name = "Asad";
+$age = 18;
+$score = 89.75;
+$is_logged_in = true;
+
+echo "Name: $name<br>";
+print "Age: $age<br>";
+echo "Score: $score<br>";
+
+if ($is_logged_in) {
+    echo "Logged in? Yes<br>";
+} else {
+    echo "Logged in? No<br>";
+}
+?>
